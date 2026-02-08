df0c424b9d601fbc2248f884e1e095c6d1eb192e
Antania <antaniaburgess59@gmail.com>
2026-02-08 16:41:28 -0500

    is the only file that has been modified. The other files have been deleted, so they will not be included in this commit.

---
+++ Practice 1.cs
@@ -0,0 +1,4 @@
+using System;
+
+int x = 12 * 30;      //Statement 1
+Console.WriteLine(x); //Statement 2
---
+++ practice5.cs
@@ -0,0 +1,5 @@
+// If you really want to use a keyword as an identifier, you can do so with the @ prefix.
+// This can be useful for language interoperability.
+
+int @class = 123;
+string @namespace = "foo";
